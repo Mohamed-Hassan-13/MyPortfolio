87eabfa4924262aef9cf1f790c8b83e3f152163a
Hamo <moahmedhassan21700@gmail.com>
2025-04-26 13:19:52 +0300

    'PortfolioVr1'

---
+++ Components/Grid.tsx
@@ -0,0 +1,26 @@
+import { gridItems } from "@/Data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item, index) => (
+          <BentoGridItem
+            key={index}
+            title={item.title}
+            id={item.id}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
---
+++ Data/index.ts
@@ -0,0 +1,191 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Experience", link: "#experience" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a JS Animation library",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/apple.png",
+      iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/gsap.svg","/vite.svg"],
+      link: "https://github.com/Mohamed-Hassan-13/Apple-iPhone",
+    },
+    {
+      id: 2,
+      title: "Swoo - E-commerce Platform",
+      des: "A modern e-commerce platform built with React and Strapi, featuring a user-friendly interface, product management, and smooth browsing experience.",
+      img: "/swoo.png",
+      iconLists: ["re.svg","boot.svg","strapi.svg"],
+      link: "https://github.com/Mohamed-Hassan-13/swoo",
+    },
+    {
+      id: 3,
+      title: "Brainwave - AI Landing Page",
+      des: "A modern, responsive AI landing page built with React.js and Tailwind CSS, featuring smooth animations and a clean design.",
+      img: "/brainwave.png",
+      iconLists: ["/re.svg", "/tail.svg", "/vite.svg", "/c.svg"],
+      link: "https://github.com/Mohamed-Hassan-13/brainwave",
+    },
+    {
+      id: 4,
+      title: "M-H Store - Full-Stack E-commerce App",
+      des: "A full-stack e-commerce app with React, Tailwind CSS, and Laravel, featuring a complete admin dashboard and user management.",
+      img: "/mhstore.png",
+      iconLists: ["/re.svg","boot.svg","/gsap.svg","laravel.svg"],
+      link: "https://github.com/Mohamed-Hassan-13/M-H-Store",
+    },
+    {
+      id: 5,
+      title: "Nike Store - E-commerce Landing Page",
+      des: "A stylish Nike e-commerce landing page built with React and Tailwind CSS, showcasing products with a sleek and responsive design.",
+      img: "/nike.png",
+      iconLists: ["/re.svg","/tail.svg","/vite.svg"],
+      link: "https://github.com/Mohamed-Hassan-13/nike",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Frontend Developer - JSM Tech",
+      desc: "Contributed to the development of responsive web applications, focusing on delivering high-quality user interfaces using React and modern frontend technologies.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const skilsLeft = [
+    {
+      id: 1,
+      title: "Redux-Toolkit",
+    },
+    {
+      id: 2,
+      title: "Next.js",
+    },
+    {
+      id: 3,
+      title: "Typescript",
+    },
+  ];
+  export const skilsRight = [
+    {
+      id: 1,
+      title: "React.js",
+    },
+    {
+      id: 2,
+      title: "Tailwind",
+    },
+    {
+      id: 3,
+      title: "Strapi",
+    },
+  ];
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      href:"https://github.com/Mohamed-Hassan-13"
+    },
+    {
+      id: 2,
+      img: "./facebook.svg",
+      href:"https://www.facebook.com/profile.php?id=100016187792954"
+
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      href:"https://www.linkedin.com/in/mohamed-hassan-774207314/"
+    },
+  ];
